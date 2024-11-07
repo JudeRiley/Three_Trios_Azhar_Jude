2d883e2deb9c3fb7d329b2c4540e70ffdfc636ea
Azhar Abdulla <azhar.abdulla005@gmail.com>
2024-11-06 19:56:51 -0500

    Refactored Model Interface

--- src/cs3500/model/ThreeTrios.java
+++ src/cs3500/model/ThreeTrios.java
@@ -1,20 +1,18 @@
 package cs3500.model;
 
-import java.util.List;
-
 /**
  * Represents a rules-keeper for a game of ThreeTrios.
  */
-public interface ThreeTrios {
+public interface ThreeTrios extends ReadOnlyThreeTrios {
 
   /**
    * Places the card at the given index in the player's hand whose turn it currently is
    * at the given position on the grid.
    *
-   * @param pos the position in the grid to place the card at.
+   * @param pos     the position in the grid to place the card at.
    * @param cardIdx an index in the current players hand.
    */
-  public void placeCard(GridPos pos, int cardIdx);
+  void placeCard(GridPos pos, int cardIdx);
 
   /**
    * A recursive function that "attacks" the neighbors of the card at the given position.
@@ -27,34 +25,5 @@ public interface ThreeTrios {
    * @param pos the position of the attacking card.
    * @return the Player whose turn is after the current turn.
    */
-  public Player battleStep(GridPos pos);
-
-  /**
-   * Returns the Player who owns the most cards only when the game is over.
-   *
-   * @return the winning Player.
-   */
-  public Player getWinner();
-
-  /**
-   * Returns the Player whose turn it is currently.
-   *
-   * @return the current turn.
-   */
-  public Player getTurn();
-
-  /**
-   * Returns a copy of the grid's state at the time this method is called.
-   *
-   * @return a 2d array representing the current grid.
-   */
-  public Cell[][] getCurrentGrid();
-
-  /**
-   * Returns the hand of the specified Player.
-   *
-   * @param player the Player who owns the desired hand.
-   * @return A list containing all the Cards in the desired hand.
-   */
-  public List<Card> getHand(Player player);
+  Player battleStep(GridPos pos);
 }
